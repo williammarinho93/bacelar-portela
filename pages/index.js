be5ad438ee5dea9644c399033e9285b3d80472f0
williammarinho93 <willmarinhosantos93@gmail.com>
2022-11-02 15:20:08 -0300

    commit

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
-
+import artur from './imagem/artur.png';
+import Image from 'next/image';
 
 
 export default function Home() {
   return (
     <div>
-      <h1>William</h1>
+      <h1>ARTHUR GARCIA GASPAR</h1>
+      <Image src={artur} alt="artur"/>
+      
     </div>
   )
 }
