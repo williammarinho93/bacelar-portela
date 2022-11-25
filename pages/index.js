c5e9f3b8f5612631bc9f42bd822b06bb4af3745e
williammarinho93 <willmarinhosantos93@gmail.com>
2022-11-25 16:45:48 -0300

    fotos

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,15 @@
-import artur from './imagem/artur.png';
+import Imagem1 from './imagem/Imagem1.jpg';
+import Imagem2 from './imagem/Imagem2.jpg';
 import Image from 'next/image';
 
 
 export default function Home() {
   return (
     <div>
-      <h1>ARTHUR GARCIA GASPAR</h1>
-      <Image src={artur} alt="artur"/>
+      
+      
+      <Image src={Imagem1} alt="Imagem1"/>
+      <Image src={Imagem2} alt="Imagem2"/>
       
     </div>
   )
